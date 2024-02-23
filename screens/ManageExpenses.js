8d370b01754dbd57937af864a556f489b9472564
Jenna de Beer <jennadebeer.01@gmail.com>
2024-02-23 16:06:16 +0200

    Building a custom input component.

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,5 @@
 import { useLayoutEffect, useContext } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 
 import Button from "../components/UI/Button";
 import IconButton from "../components/UI/IconButton";
