dd762925572890e576241daa79ac19efc6fe5c37
Jenna de Beer <jennadebeer.01@gmail.com>
2024-02-23 15:26:47 +0200

    Managing app-wide state with context, using context from inside components.

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,44 +4,11 @@ import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "Petrol",
-    amount: 400.0,
-    date: new Date("2024-02-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of Demonias",
-    amount: 3499.99,
-    date: new Date("2024-02-21"),
-  },
-  {
-    id: "e3",
-    description: "Groceries",
-    amount: 650.00,
-    date: new Date('2024-02-23')
-  },
-  {
-    id: "e4",
-    description: "Palworld",
-    amount: 270.00,
-    date: new Date("2024-02-25"),
-  },
-  {
-    id: "e5",
-    description: "Tokyo Ghoul box set",
-    amount: 270.00,
-    date: new Date('2024-02-27')
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container} >
-      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
-      <ExpensesList expenses={DUMMY_EXPENSES} />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+      <ExpensesList expenses={expenses} />
     </View>
   );
 }
